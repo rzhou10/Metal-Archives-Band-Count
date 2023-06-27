76aedb30291d58837ec4a4f88b421864b5ae955c
rzhou10 <rzhou10@gmail.com>
2023-06-27 19:24:42 -0400

    refactored to just run as script

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 import bandCount
 
-def main():
-    while True:
-        choice = input("Hello, what would you like to count by? Type \"help\" for all options.")
-        if choice == "leave":
-            print("Goodbye!")
-            break
-        elif choice == "help":
-            print("The choices are:\n\"leave\"\n\"country\"\n\"alphabet\"\n\"genre\"")
-        elif choice == "country":
-            bandCount.countBands("country", "bandListCountry_info")
-        elif choice == "alphabet":
-            bandCount.countBands("alphabet", "bandListAlpha_info")
-        elif choice == "genre":
-            bandCount.countBands("genre", "bandListGenre_info")
-        else:
-            print("Not a valid choice")
-    
-if __name__ == "__main__":
-    main()
-    exit()
+print("fetching countries")
+bandCount.countBands("country", "bandListCountry_info")
+
+print("fetching alphabet")
+bandCount.countBands("alphabet", "bandListAlpha_info")
+
+print("fetching genres")
+bandCount.countBands("genre", "bandListGenre_info")
+
+print("done fetching")
